9c6749c68c5ab31143de518a295be9d981bd04b2
khoaxavi123 <nguyendangkhoathienke@gmail.com>
2024-04-14 21:46:16 +0700

    hello 500 ae

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "subjects#index"
+
+  resources :books
+  resources :subjects
+  resources :customers
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+end
